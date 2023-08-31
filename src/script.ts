4b31ec0803c020feb1f000f88447147c43a218de
George Leyva <leyvageorge@hotmail.com>
2023-08-30 18:19:22 -0700

    added carousel

--- src/script.ts
+++ src/script.ts
@@ -2,26 +2,26 @@ let slideIndex:number = 1;
 
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function plusSlides(n:number) {
     showSlides(slideIndex +=n);
 }
 
-function currentSlide(n) {
+function currentSlide(n:number) {
     showSlides(slideIndex = n)
 }
 
-function showSlides(n) {
+function showSlides(n:number) {
     let i;
     let slides = document.getElementsByTagName('mySlides');
     let dots = document. getElementsByClassName('dot');
     if(n > slides.length) {slideIndex = 1}
     if(n < 1) {slideIndex = slides.length}
     for(i = 0; i < slides.length;i++){
-        slides[i].style.display = 'none';
+        (slides[i] as HTMLElement).style.display = 'none';
     }
     for(i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(' active', "");
     }
-    slides[slideIndex-1].style.display= 'block';
+    (slides[slideIndex-1] as HTMLElement).style.display= 'block';
     dots[slideIndex-1].className += ' active';
 }
