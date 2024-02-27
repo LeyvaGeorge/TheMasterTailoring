2d177853f3821b69360e11a3b9097866cdfd80d3
George Leyva <leyvageorge@hotmail.com>
2024-02-27 10:49:42 -0800

    images and format for alteration discript

--- src/script.ts
+++ src/script.ts
@@ -53,34 +53,3 @@ async function initMap(): Promise<void> {
 }
 
 initMap();
-
-//Slides for carosell 
-// let slideIndex:number = 1;
-
-// showSlides(slideIndex);
-
-// function plusSlides(n:number) {
-//     showSlides(slideIndex +=n);
-// }
-
-// function currentSlide(n:number) {
-//     showSlides(slideIndex = n)
-// }
-
-// function showSlides(n:number) {
-//     let i;
-//     let slides = document.getElementsByTagName('mySlides');
-//     let dots = document. getElementsByClassName('dot');
-//     if(n > slides.length) {slideIndex = 1}
-//     if(n < 1) {slideIndex = slides.length}
-//     for(i = 0; i < slides.length;i++){
-//         (slides[i] as HTMLElement).style.display = 'none';
-//     }
-//     for(i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(' active', "");
-//     }
-//     (slides[slideIndex-1] as HTMLElement).style.display= 'block';
-//     dots[slideIndex-1].className += ' active';
-// }
-
-
